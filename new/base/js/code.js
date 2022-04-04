6c35799f687c8570b51eac31bdc10334331525b2
Sow <87394436+Sowzz@users.noreply.github.com>
2022-04-04 18:46:20 +0200

    search bar 100% fonctionelle

--- new/base/js/code.js
+++ new/base/js/code.js
@@ -1,32 +1,11 @@
-const persons = [
-
-    {name:'Cybertrunck' ,categorie:'action'},
-    {name:'Cybersex' ,categorie:'sex'},
-    {name:'cybertruc' ,categorie:'rpg'},
-    {name:'cybercool' ,categorie:'freedom'},
-    {name:'cybermilf' ,categorie:'ph'},
-    {name:'vichoucroute' ,categorie:'baka'},
-    {name:'miam' ,categorie:'UwU'},
-    {name:'admin' ,categorie:''},
-    {name:'CyberPunk 2077' ,categorie:'action'},
+let suggestions = [
+    "yo",
+    "cybertrunk",
+    "cyberpunk",
+    "cyberattack",
+    "ddos",
+    "vichoucroute UwU Channel",
+    "i love u all",
+    "<3",
+    "gta"
 ];
-
-const searchinput = document.getElementById('searchInput');
-
-searchinput.addEventListener('keyup', function(){
-    const input = searchinput.value;
-
-    const result = persons.filter(item => item.name.toLocaleLowerCase().includes(input.toLocaleLowerCase()));
-
-    let suggestions = '';
-
-    if(input !=''){
-    result.forEach(resultItem =>
-        suggestions +=`
-            <div class="suggestions">${resultItem.name}</div>
-            `
-        )
-    }
-
-    document.getElementById('suggestions').innerHTML = suggestions;
-})
